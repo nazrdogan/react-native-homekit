9853d06c1fde3f48f59a25f5d8afcb1f2f777a45
Nazir Dogan <nazrdogan@gmail.com>
2020-06-20 16:02:25 +0300

    add TS types

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,17 @@ type HomekitType = {
   addZone(zoneName: string, toHome: string): Promise<string>;
   removeZone(zoneName: string, fromHome: string): Promise<string>;
   renameZone(newName: string, oldName: string, inHome: string): Promise<string>;
+
+  addRoomToHome(name: string, toHome: string): Promise<string>;
+  addRoomToZone(name: string, toZone: string, inHome: string): Promise<string>;
+  removeRoomFromZone(name: string, fromZone: string, inHome: string): Promise<string>;
+  removeRoomFromHome(name: string, fromHome: string): Promise<string>;
+  renameRoom(oldName: string, newName: string, inHome: string): Promise<string>;
+
+  addAccessoryToHome(accessoryName: string, toHome: string): Promise<string>;
+  removeAccessoryFromHome(accessoryName: string, fromHome: string): Promise<string>;
+  assignAccessoryToRoom(accessoryName: string, roomName: string, homeName: string): Promise<Array<string>>;
+  renameAccessory(oldName: string, newName: string): Promise<string>;
 };
 
 const { Homekit } = NativeModules;
